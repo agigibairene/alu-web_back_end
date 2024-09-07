ede72cf4dd4fabcbab9bccf60ac12efc04313b08
root <i.agigiba@alustudent.com>
2024-09-07 16:47:21 +0000

    Python - Async Comprehension

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -6,7 +6,7 @@ from typing import Generator
 
 
 async def async_generator() -> Generator[float, None, None]:
-    '''will loop 10 times, each time wait 1s, return a random number between 0 and 10'''
+    ''' yields or returns a random number between 0 and 10'''
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.random() * 10
--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -9,7 +9,7 @@ async_comprehension = using('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
-    '''Executes async_comprehension 4 times and measures the total execution time.'''
+    '''measure_runtime should measure the total runtime and return it'''
     start_time = time.time()
     await asyncio.gather(*(async_comprehension() for _ in range(4)))
     end_time = time.time()
