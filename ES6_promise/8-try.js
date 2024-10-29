bfa837c3d8b92a7db711587191e3f34e32bc62dd
agigibairene <i.agigiba@alustudent.com>
2024-10-29 13:49:52 +0200

    ES6

--- ES6_promise/8-try.js
+++ ES6_promise/8-try.js
@@ -1,10 +1,10 @@
 export default function divideFunction(numerator, denominator) {
-    try {
-      if (denominator === 0) {
-        throw new Error('cannot divide by 0');
-      }
-        return numerator / denominator;
-    } catch (error) {
-        throw error;
+  try {
+    if (denominator === 0) {
+      throw new Error('cannot divide by 0');
     }
-};
+    return numerator / denominator;
+  } catch (error) {
+    throw error;
+  }
+}
