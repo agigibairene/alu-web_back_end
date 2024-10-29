e9018b94af6751ae5e1670d0d662ab1eb1490091
agigibairene <i.agigiba@alustudent.com>
2024-10-29 12:08:47 +0200

    ES6

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,10 +1,14 @@
 export default function createReportObject(employeesList) {
+  const allEmployees = {};
+
+  for (const [department, employees] of Object.entries(employeesList)) {
+    allEmployees[department] = [...employees];
+  }
+
+  const getNumberOfDepartments = () => Object.keys(employeesList).length;
+
   return {
-    allEmployees: {
-      ...employeesList,
-    },
-    getNumberOfDepartments() {
-      return Object.keys(employeesList).length;
-    },
+    allEmployees,
+    getNumberOfDepartments,
   };
 }
